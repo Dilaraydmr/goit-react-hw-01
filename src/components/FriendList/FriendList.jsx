473a872ed7f6658dd6dc29c904ab21f88bf22ea2
dilaraydmr <dilaraaydemir1@gmail.com>
2025-04-23 21:27:14 +0300

    commitmsg

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import FriendListCss from './FriendList.module.css'
+import FriendListItem from '../FriendListItem/FriendListItem'
+
+const FriendList = ({ friends }) => {
+    return (
+        <ul>
+            {friends.map((friend) => {
+                return (
+                    <FriendListItem
+                        key={friend.id}
+                        avatar={friend.avatar}
+                        name={friend.name}
+                        isOnline={friend.isOnline}
+                    />
+
+
+
+
+                )
+            }
+            )}
+        </ul>
+
+
+    )
+}
+
+export default FriendList
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import * as Clara from './Transaction.module.css';
+
+const TransactionHistory = ({ items }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map((item, index) => {
+                    return (
+                        <tr key={index}>
+
+                            <td>{item.type}</td>
+                            <td>{item.amount}</td>
+                            <td>{item.currency}</td>
+                        </tr>
+                    )
+                }
+
+                )}
+            </tbody>
+        </table>
+    )
+}
+
+export default TransactionHistory
